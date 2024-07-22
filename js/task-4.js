73abb31d539c55a67f0f2bb929b3d223e28e973e
Lena_Bondar <lencha.2005@gmail.com>
2024-07-22 12:09:45 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -4,8 +4,8 @@ const onFormSubmit = event => {
     event.preventDefault();
 
     const formData = {
-        email: formEl.elements.email.value,
-        password: formEl.elements.password.value,
+        email: formEl.elements.email.value.trim(),
+        password: formEl.elements.password.value.trim(),
     };
 
     if(formEl.elements.email.value === '' && formEl.elements.password.value === ''){
